c64a17bd2fc070692177560e3187ff55d63e7eef
Anina Angelova <anina_1999@abv.bg>
2026-07-28 20:23:23 +0300

    Setup express handlebars

--- README.md
+++ README.md
@@ -7,5 +7,6 @@
  - [x] Add Express Server and Express Handlebars  `npm i express express-handlebars`
  - [x] Add .gitignore
  - [x] Add resources
+ - [x] Setup express handlebars
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
 
+//Setup handlebars as the view engine
+app.engine('hbs', engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+
+//Routes
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 });
